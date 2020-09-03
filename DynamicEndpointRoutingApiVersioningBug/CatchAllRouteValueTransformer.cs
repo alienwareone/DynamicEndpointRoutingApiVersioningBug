c86f72f5475dff1560da10c4796fe4dac4b5e91d
alienwareone <beni@mailboxes.ch>
2020-09-03 18:28:16 +0200

    Projektdateien hinzufügen.

---
+++ DynamicEndpointRoutingApiVersioningBug/CatchAllRouteValueTransformer.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.Routing;
+using System.Threading.Tasks;
+
+namespace DynamicEndpointRoutingApiVersioningBug
+{
+    public class CatchAllRouteValueTransformer : DynamicRouteValueTransformer
+    {
+        public override async ValueTask<RouteValueDictionary> TransformAsync(HttpContext httpContext, RouteValueDictionary values)
+        {
+            await Task.CompletedTask;
+
+            if (httpContext.Request.Path == "/Test")
+            {
+                return new RouteValueDictionary(new { controller = "Home", action = "CatchAll" });
+            }
+
+            return null;
+        }
+    }
+}
